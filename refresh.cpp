f87e6c6a0490febf465aa147d51dfe1f462f26b0
xiayanzhiyouli <xiayanasdfg@163.com>
2022-09-26 16:57:23 +0800

    Nearly finished

--- refresh.cpp
+++ refresh.cpp
@@ -10,11 +10,36 @@
 
 #include <unistd.h>
 #include "refresh.h"
+#include "time.h"
 
 using namespace std;
 //the unit of time is the second.
 
-bool my_JudgeExpire(string& a,string& b)
+extern Time* p2te;
+
+bool IntentionSmaller(const intention& a,const intention& b)
+{
+    if(a.price < b.price || (a.price == b.price && a.buy_time > b.buy_time))
+        return true;
+    return false;
+}
+
+void IntentionSwap(intention& a,intention& b)
+{
+    intention temp = a;
+    a = b;
+    b = temp;
+}
+
+void IntentionSort(intention* p,int n)
+{
+    for(int i = n;i > 0;--i)
+        for(int j = 0;j < i-1;++j)
+            if(IntentionSmaller(p[j],p[j + 1]))
+                IntentionSwap(p[j],p[j +1]);
+}
+
+bool my_JudgeExpire(const string& a,const string& b)
 {
     if(my_Compare(a,b) > INTERVAL)
         return true;//expired.
@@ -22,7 +47,7 @@ bool my_JudgeExpire(string& a,string& b)
         return false;//not expired.
 
 }
-int my_Compare(string& a,string& b)
+int my_Compare(const string& a,const string& b)
 {
     compare_date a_date = my_Split(a);
     compare_date b_date = my_Split(b);
@@ -36,7 +61,7 @@ int my_Compare(string& a,string& b)
     return abs(res);
 }
 
-compare_date my_Split(string& a)
+compare_date my_Split(const string& a)
 {
     int temp[5];
     int cnt = 0;
@@ -66,7 +91,7 @@ string my_Combine(compare_date& a)
     return res;
 }
 
-bool my_Earlier(string& a,string& b)
+bool my_Earlier(const string& a,const string& b)
 {
     compare_date a_date = my_Split(a);
     compare_date b_date = my_Split(b);
@@ -98,7 +123,7 @@ bool my_Earlier(string& a,string& b)
     return true;
 }
 
-void my_Deduction(string& a,double n)
+void my_Deduction(const string& a,double n)
 {
     ifstream in("user_info.txt");
     if (!in.is_open())
@@ -148,7 +173,57 @@ void my_Deduction(string& a,double n)
     out.close();
 }
 
-double my_CheckBalance(string& a)
+void my_Remittance(const string& a,double n)
+{
+    ifstream in("user_info.txt");
+    if (!in.is_open())
+    {
+        cout << "Error opening file"<<endl; 
+        in.close();
+        return;
+    }
+
+    string id,name,tele,password,balance,state,all_content;
+
+    while (!in.eof() )
+    {
+        string buffer1,buffer2;
+        getline(in,buffer1);// main info except address;
+        getline(in,buffer2);//address
+        istringstream is(buffer1);
+
+        is >> id;//id
+        if(id == a)
+        {
+            all_content += id; all_content += " ";
+            is >> name; all_content += name; all_content += " ";
+            is >> tele; all_content += tele; all_content += " ";
+            is >> password; all_content += password; all_content += " ";
+
+            is >> balance; 
+            double d_balance = stod(balance);
+            d_balance += n;
+            all_content += to_string(d_balance); all_content += " ";
+
+            is >> state; all_content += state; all_content += "\n";
+            all_content += buffer2; all_content += "\n";
+        }
+        else
+        {
+            all_content += buffer1; all_content += "\n";
+            all_content += buffer2; all_content += "\n";
+        }
+    }
+    in.close();
+
+    all_content = all_content.substr(0,all_content.length() -1);//delete the last '\n'.
+    ofstream out("user_info.txt");
+    out.flush();
+    out << all_content;
+    out.close();
+}
+
+double my_CheckBalance(const string& a)
 {
     double res;
     ifstream in("user_info.txt");
@@ -156,7 +231,7 @@ double my_CheckBalance(string& a)
     {
         cout << "Error opening file"<<endl; 
         in.close();
-        return;
+        return -1;
     }
 
     string id,name,tele,password,balance,state;
@@ -185,12 +260,205 @@ double my_CheckBalance(string& a)
     return res;
 }
 
+int my_CheckComNum(const string& a)
+{
+    string res;
+    ifstream in("commodity_info.txt");
+    if (!in.is_open())
+    {
+        cout << "Error opening file"<<endl;
+        return -1; 
+    }
+        
+    while (!in.eof() )
+    {
+        string buffer1,buffer2,buffer3,temp;
+        getline(in,buffer1);
+        getline(in,buffer2);
+        getline(in,buffer3);
+
+        istringstream is(buffer1);
+
+        is >> temp;
+
+        if(temp == a)
+        {
+            is >> temp; is >> res;
+            break;
+        }
+    }
+    in.close();
+
+    return stoi(res);
+}
+
 void my_ScanIntention()
 {
-    intention* p = new intention[BUFFER_SIZE];
+    intention* p = new intention[INTENTION_BUFFER_SIZE];
     int cnt = 0;
+    bool find = false;
     string com, buyer, seller, num, price, buy_time, added_time,temp;
 
-    //TO DO
+    //first ergodic of the file.
+    //find an expired commodity.
+    ifstream in("intention_info.txt");
+    if (!in.is_open())
+    {
+        cout << "Error opening file"<<endl;
+        return; 
+    }
+        
+    while (!in.eof() )
+    {
+        string buffer;
+        getline(in,buffer);
+        istringstream is(buffer);
 
+        is >> com; is >> buyer; is >> seller;
+        is >> num; is >> price; is >> buy_time; is >> added_time;
+
+        p2te->Reset();
+        if(my_JudgeExpire(added_time,p2te->GetStrAll()))//expired
+        {
+            find = true;
+            break;
+        }
+    }
+    in.close();
+    
+    //second ergodic of the file.
+    //find all the buyers of this expired commodity.
+    string all_content;
+    ifstream collect("intention_info.txt");
+    if (!collect.is_open())
+    {
+        cout << "Error opening file"<<endl;
+        return; 
+    }
+        
+    while (!collect.eof() )
+    {
+        string buffer,temp;
+        getline(collect,buffer);
+        istringstream is(buffer);
+
+        is >> temp;
+
+        if(temp == com)
+        {
+            p[cnt].com_id = temp;
+            is >> temp; p[cnt].buyer_id = temp;
+            is >> temp; p[cnt].seller_id = temp;
+            is >> temp; p[cnt].num = stoi(temp);
+            is >> temp; p[cnt].price = stod(temp);
+            is >> temp; p[cnt].buy_time = temp;
+            is >> temp; p[cnt].added_time = temp;
+
+            if(p[cnt].num * p[cnt].price <= my_CheckBalance(p[cnt].buyer_id))
+                ++cnt;
+        }
+        else
+        {
+            all_content += buffer; all_content += "\n";
+        }
+    }
+    collect.close();
+
+    all_content = all_content.substr(0,all_content.length() -1);//delete the last '\n'.
+
+    ofstream out("intention_info.txt");
+    out.flush();
+    out << all_content;
+    out.close();
+    
+    IntentionSort(p,cnt);
+    //sort all the buyers.
+    //bigger -> smaller.
+
+    int sum = 0,deal_num = 0;
+    for(int i = 0;i < cnt;++i)
+    {
+        sum += p[i].num;
+        if(sum < stoi(num))
+            ++deal_num;
+    }
+
+    UpdateUser(p,deal_num);
+    UpdateCom(p,deal_num);
+    UpdateOrder(p,deal_num);
+    //intention_info.txt has been updated.
+}
+
+void my_ReduceComNum(const string& a, int n)
+{
+    ifstream in("commodity_info.txt");
+    if (!in.is_open())
+    {
+        cout << "Error opening file"<<endl; 
+        in.close();
+        return;
+    }
+
+    string id,price,num,seller,added_date,state,name,description,all_content;
+
+    while (!in.eof() )
+    {
+        string buffer1,buffer2,buffer3;
+        getline(in,buffer1);
+        getline(in,buffer2);
+        getline(in,buffer3);
+        istringstream is(buffer1);
+
+        is >> id;//id
+        if(id == a)
+        {
+            all_content += id; all_content += " ";
+            is >> price; all_content += price; all_content += " ";
+            is >> num; 
+            int reduce_num = stoi(num);
+            reduce_num -= n;
+            all_content += to_string(reduce_num); all_content += " ";
+            is >> seller; all_content += seller; all_content += " ";
+            is >> added_date; all_content += added_date; all_content += " ";
+            is >> state; all_content += state; all_content += "\n";
+
+            all_content += buffer2; all_content += "\n";
+            all_content += buffer3; all_content += "\n";
+        }
+        else
+        {
+            all_content += buffer1; all_content += "\n";
+            all_content += buffer2; all_content += "\n";
+            all_content += buffer3; all_content += "\n";
+        }
+    }
+    in.close();
+
+    all_content = all_content.substr(0,all_content.length() -1);//delete the last '\n'.
+    ofstream out("commodity_info.txt");
+    out.flush();
+    out << all_content;
+    out.close();
+}
+
+void UpdateUser(intention* p,int cnt)
+{
+    for(int i = 0;i < cnt;++i)
+    {
+        my_Deduction(p[i].buyer_id,p[i].price * p[i].num);
+        my_Remittance(p[i].seller_id,p[i].price * p[i].num);
+    }
+    return;
+}
+
+void UpdateCom(intention* p,int cnt)
+{
+    for(int i = 0;i < cnt;++i)
+        my_ReduceComNum(p[i].com_id,p[i].num);
+    return;
+}
+
+void UpdateOrder(intention* p,int cnt)
+{
+    return;
 }
--- refresh.h
+++ refresh.h
@@ -4,7 +4,7 @@
 using namespace std;
 
 #define INTERVAL 300
-#define BUFFER_SIZE 100
+#define INTENTION_BUFFER_SIZE 100
 
 struct compare_date
 {
@@ -20,25 +20,50 @@ struct intention
     string added_time;
 };
 
-bool my_JudgeExpire(string& a,string& b);
+bool IntentionSmaller(const intention& a,const intention& b);
+//return true if a is "really smaller" than b, false otherwise.
+//a is "really smaller" than b if a.price < b.price || (a.price == b.price && a.buy_time > b.buy_time).
+
+void IntentionSwap(intention& a,intention& b);
+
+void IntentionSort(intention* p,int n);
+//bigger -> smaller.
+
+bool my_JudgeExpire(const string& a,const string& b);
 //to judge whether the commodity is expired.
 
-int my_Compare(string& a,string& b);
+int my_Compare(const string& a,const string& b);
 //return the abs of the time between a and b, with the unit of sec.
 
-compare_date my_Split(string& a);
+compare_date my_Split(const string& a);
 //split the string standing for time into a struct called compare_date.
 
-string my_Combine(compare_date& a);
+string my_Combine(const compare_date& a);
 //combine compare_date back to a string.
 
-bool my_Earlier(string& a,string& b);
+bool my_Earlier(const string& a,const string& b);
 // return true if a is earlier than b, false otherwise.
 
-void my_Deduction(string& a,double n);
+void my_Deduction(const string& a,double n);
+//deduce buyer's balance after the trade was done.
 
-double my_CheckBalance(string& a);
+void my_Remittance(const string& a,double n);
+//remit buyer's balance after the trade was done.
+
+double my_CheckBalance(const string& a);
+//check buyer's balance before the trade is done.
+
+int my_CheckComNum(const string& a);
+//chek commodity's num before the trade is done.
 
 void my_ScanIntention();
+//only choose an expired commodity to trade once.
 
+void my_ReduceComNum(const string& a, int n);
+//reduce commodity's num after the trade was done.
+
+void UpdateUser(intention* p,int cnt);
+void UpdateCom(intention* p,int cnt);
+void UpdateOrder(intention* p,int cnt);
+//three funtions are used for updating information in their own txt file.
 #endif
